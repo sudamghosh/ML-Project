3d710ce435d86d883100a6bb7eb0330ce056b94a
sudamghosh <sudamghosh@gmail.com>
2022-06-25 19:52:42 +0530

    message

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -17,3 +17,5 @@ CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
 TRAINING_PIPELINE_CONFIG_KEY = "training_pipeline_config"
 TRAINING_PIPELINE_ARTIFACT_DIR_KEY = "artifact_dir"
 TRAINING_PIPELINE_NAME_KEY = "pipeline_name"
+
+#Data Ingestion Related Variable
